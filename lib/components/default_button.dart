7f0ded0d7f4a4a4f19f4a31ac38476138a251c22
aliarridla <beeunik0@gmail.com>
2022-01-13 12:59:48 +0700

    error buk

--- lib/components/default_button.dart
+++ lib/components/default_button.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 import '../constants.dart';
 import '../size_config.dart';
 
-
-
 class DefaultButton extends StatelessWidget {
   const DefaultButton({
     Key? key,
@@ -18,11 +16,11 @@ class DefaultButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      height: getProportionateScreenHeight(56),
+      height: getProportionateScreenHeight(62),
       child: TextButton(
         style: TextButton.styleFrom(
           shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
           primary: Colors.white,
           backgroundColor: kPrimaryColor,
         ),
@@ -30,11 +28,30 @@ class DefaultButton extends StatelessWidget {
         child: Text(
           text!,
           style: TextStyle(
-            fontSize: getProportionateScreenWidth(18),
+            fontSize: getProportionateScreenWidth(20),
             color: Colors.white,
+            fontFamily: 'Larsseit',
           ),
         ),
       ),
     );
+
+  //   return Container(
+  //     width: 310,
+  //     height: 58,
+  //     decoration: const BoxDecoration(
+  //       borderRadius: BorderRadius.all(Radius.circular(16)),
+  //       color: Color(0xff2f539d),
+  //     ),
+  //     child: const Text(
+  //       'Get Start',
+  //       style: TextStyle(
+  //         color: Colors.white,
+  //         fontSize: 20,
+  //         fontFamily: 'Larsseit',
+  //         fontWeight: FontWeight.w500,
+  //       ),
+  //     ),
+  //   );
   }
 }
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,16 +1,14 @@
-import 'dart:js';
-
 import 'package:flutter/widgets.dart';
-import 'package:reka/screens/Welcome/welcome_screen.dart';
 import 'package:reka/screens/loading_screen.dart';
+import 'package:reka/screens/login/login_screen.dart';
 // import 'package:reka/screens/splash/splash_screen.dart';
 
 
 // We use name route
 // All our routes will be available here
 final Map<String, WidgetBuilder> routes = {
-  WelcomeScreen.routeName: (context) => WelcomeScreen(),
+  // WelcomeScreen.routeName: (context) => WelcomeScreen(),
   // ignore: prefer_const_constructors
   LoadingScreen.routeName: (context) => LoadingScreen(),
-  
+  LoginScreen.routeName: (context) => LoginScreen(),
 };
