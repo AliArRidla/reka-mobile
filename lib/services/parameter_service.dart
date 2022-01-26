1643a1ceb4b0dd9d990a93ef27bcf179731765e2
aliarridla <beeunik0@gmail.com>
2022-01-26 20:25:42 +0700

    add parameter service

---
+++ lib/services/parameter_service.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:reka/models/machine_model.dart';
+import 'package:reka/models/parameter_model.dart';
+
+class ParameterService {
+  String baseUrl = 'http://127.0.0.1:8000/api';
+
+  Future<List<ParameterModel>> getParameters() async {
+    var url = '$baseUrl/parameters';
+    var headers = {'Content-Type': 'application/json'};
+    var response = await http.get(url, headers: headers);
+
+    print(response.body);
+
+    if (response.statusCode == 200) {
+      List data = jsonDecode(response.body)['data'];
+      List<ParameterModel> parameters = [];
+
+      for (var item in data) {
+        parameters.add(ParameterModel.fromJson(item));
+      }
+
+      return parameters;
+    }else {
+      throw Exception("Gagal get data machines!");
+    }
+  }
+}
